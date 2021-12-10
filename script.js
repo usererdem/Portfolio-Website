620fbb4b89068223a02ff416f76b540c0333e566
Erdem Elvan <erdemelvan35@gmail.com>
2021-12-10 14:04:23 +0300

    nav menu for mobile devices added, first commit for it

--- script.js
+++ script.js
@@ -57,3 +57,11 @@ window.addEventListener("scroll", () => {
   }
 });
 
+/* Navigation menu for mobile devices */
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navigation__menu');
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+})
